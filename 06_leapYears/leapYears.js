5924f277904a59bb4d1a6d00cd39f08988f667b5
Andrea Nichele <a.nichele96@gmail.com>
2022-03-14 11:20:24 +0100

    yo

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+let year = 700;
 
+const leapYears = function(year) {
+    if(year%400 == 0){
+        return true;
+    }else if(year%100 == 0){
+        return false;
+    }else if(year%4 == 0){
+        return true;
+    }else{
+        return false;
+    }
 };
+
+//console.log(leapYears(year));
 
 // Do not edit below this line
 module.exports = leapYears;
